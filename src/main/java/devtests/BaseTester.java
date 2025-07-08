9a9c3749bec503d47d6cea8b1d48dd87d0d51d35
Jack Park <jackpark@gmail.com>
2025-07-08 16:21:03 -0700

    adding base

---
+++ src/main/java/devtests/BaseTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package devtests;
+
+import org.topicqucsts.asr.nlp.HarvestEnvironment;
+
+/**
+ * @author jackpark
+ */
+public class BaseTester {
+	protected HarvestEnvironment environment;
+	/**
+	 * 
+	 */
+	public BaseTester() {
+		environment = new HarvestEnvironment();
+	}
+
+}
